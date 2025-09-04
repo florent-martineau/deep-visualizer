0eab9f362f0d2bae52e40e90c288e7ff298d9870
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-04 14:25:05 +0200

    Ensure dependencies must be pinned to a specific minor version (#52)

---
+++ api/scripts/check_dependencies.py
@@ -0,0 +1,24 @@
+import tomllib
+import sys
+
+class DependencyNotPinnedError(Exception):
+    def __init__(self, dependency):            
+        super().__init__(f"Dependency {dependency} should be pinned using either ~= or ==")
+            
+def check_dependencies():
+    with open("pyproject.toml", "rb") as f:
+        data = tomllib.load(f)
+    
+    dependencies = data.get("project", {}).get("dependencies", [])
+
+    dependency_groups = data.get("dependency-groups", {})
+    for deps in dependency_groups.values():
+        dependencies += deps
+    
+    for dependency in dependencies:
+        if not ("~=" in dependency or "==" in dependency):
+            raise DependencyNotPinnedError(dependency=dependency)
+
+if __name__ == "__main__":
+    if not check_dependencies():
+        sys.exit(1)
