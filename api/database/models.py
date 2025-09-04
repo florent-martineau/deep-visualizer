75a3dd63386cc39aaefec9222e6353ecc23e8db3
Florent Martineau <ftecmartineau@gmail.com>
2025-09-04 10:36:44 +0200

    Create table on startup based on Hero model

--- api/database/models.py
+++ api/database/models.py
@@ -1,6 +1,5 @@
 from sqlmodel import Field, SQLModel
 
-
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
--- api/main.py
+++ api/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 from .routers import inference
+from .database.setup import create_db_and_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(inference.router)
