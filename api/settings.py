4f0e3db5923437be5a53d16dc2253603d9044caa
Florent Martineau <ftecmartineau@gmail.com>
2025-09-07 10:46:22 +0200

    Environment variables: use Settings

--- api/settings.py
+++ api/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     betterstack_token: str
     betterstack_ingesting_host: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
