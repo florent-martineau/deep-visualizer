d3e7ed7e811281e808ba63e8cfe4d24a26b9f3a4
Florent Martineau <ftecmartineau@gmail.com>
2025-09-08 12:40:07 +0200

    Ruff: enable tidy imports

--- api/routers/inference.py
+++ api/routers/inference.py
@@ -14,5 +14,9 @@ async def hello(name: str):
 
 # @router.post("/infer")
 # async def infer():
-#     model_pipeline = pipeline("text-generation", model="openai-community/gpt2", trust_remote_code=True)
+#     model_pipeline = pipeline(
+#                       "text-generation",
+#                       model="openai-community/gpt2",
+#                       trust_remote_code=True
+#                     )
 #     return model_pipeline("Tell me something unusual", max_length=50)
