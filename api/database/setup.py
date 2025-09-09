6efee79cb9857dbd27f696288b610b35accb5d23
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-09 09:54:04 +0200

    Undo migrations (#81)

--- api/database/setup.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import SQLModel
-from .connection import engine
-
-def create_db_and_tables():
-    print("Creating database and tables")
-    SQLModel.metadata.create_all(engine)
--- api/main.py
+++ api/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
 from routers import inference
-from database.setup import create_db_and_tables
-from contextlib import asynccontextmanager
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(inference.router)
