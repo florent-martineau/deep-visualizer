0ac291ca932daae889dd12294fb3acf05596b0be
Florent Martineau <ftecmartineau@gmail.com>
2025-09-11 22:13:48 +0200

    POST  should check the validity of an access token

--- api/routers/access_token_test.py
+++ api/routers/access_token_test.py
@@ -1,9 +1,11 @@
 from http.cookies import SimpleCookie
 
+import pytest
 from fastapi.testclient import TestClient
 
 from constants import ACCESS_TOKEN_COOKIE_HEADER, ACCESS_TOKEN_COOKIE_NAME
 from main import app
+from utils.env import test_settings
 
 client = TestClient(app)
 
@@ -21,20 +23,38 @@ def make_post_request(token: str | None):
     return response, None
 
 
-VALID_TOKEN = "hf_123456789abcdefghijklmnopqrstuvwxy"
-
-
 class PostAccessToken:
-    def should_set_a_secure_httponly_cookie(self):
-        response, access_token_cookie = make_post_request(VALID_TOKEN)
+    @pytest.mark.parametrize(
+        "token",
+        [
+            test_settings().hf_access_token_fine_grained,
+            test_settings().hf_access_token_read,
+            test_settings().hf_access_token_write,
+        ],
+        ids=["fine_grained", "read", "write"],
+    )
+    def should_set_a_secure_httponly_cookie(self, token: str):
+        response, access_token_cookie = make_post_request(token)
         assert response.status_code == 200
         assert access_token_cookie is not None
         assert access_token_cookie["httponly"]
         assert access_token_cookie["secure"]
         assert access_token_cookie["samesite"] == "strict"
-        assert access_token_cookie.value == VALID_TOKEN
+        assert access_token_cookie.value == token
 
     def should_return_400_if_no_header_is_sent(self):
         response, access_token_cookie = make_post_request(None)
+        assert response.status_code == 422
+        assert access_token_cookie is None
+
+    def should_return_422_if_token_is_invalid(self):
+        response, access_token_cookie = make_post_request("hf_doesnotexist")
+        assert response.status_code == 422
+        assert access_token_cookie is None
+
+    def should_return_422_if_fine_grained_token_cannot_access_gated_repositories(
+        self,
+    ):
+        response, access_token_cookie = make_post_request("hf_doesnotexist")
         assert response.status_code == 422
         assert access_token_cookie is None
---
+++ api/utils/env.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    class Config:
+        env_file = ".env"
+
+    betterstack_token: str
+    betterstack_ingesting_host: str
+
+    @classmethod
+    def get(cls):
+        return
+
+
+settings = Settings.model_validate({})
+
+
+class TestSettings(BaseSettings):
+    class Config:
+        env_file = ".env.test"
+
+    hf_access_token_fine_grained: str
+    hf_access_token_fine_grained_without_access_to_gated_repos: str
+    hf_access_token_read: str
+    hf_access_token_write: str
+
+
+def test_settings():
+    return TestSettings.model_validate({})
