7d877f8fb98c1b586d4cfa6ae9e84cc0ce177762
Florent Martineau <ftecmartineau@gmail.com>
2025-09-16 10:09:24 +0200

    Add supported list of models

--- api/constants.py
+++ api/constants.py
@@ -1,2 +1,8 @@
 ACCESS_TOKEN_COOKIE_NAME = "access_token"
 ACCESS_TOKEN_COOKIE_HEADER = "X-Access-Token"
+SUPPORTED_MODELS = [
+    "openai-community/openai-gpt",
+    "openai-community/gpt2",
+    "openai/gpt-oss-20b",
+    "mistralai/Mistral-7B-Instruct-v0.3",
+]
