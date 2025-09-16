65951980ffa188085d782a8b3aa7704be72c9e57
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-16 11:16:38 +0200

    Make constants `Final` (#135)

--- api/core/supported_models.py
+++ api/core/supported_models.py
@@ -1,6 +1,8 @@
-SUPPORTED_MODELS = [
+from typing import Final
+
+SUPPORTED_MODELS: Final[tuple[str, ...]] = (
     "openai-community/openai-gpt",
     "openai-community/gpt2",
     "openai/gpt-oss-20b",
     "mistralai/Mistral-7B-Instruct-v0.3",
-]
+)
