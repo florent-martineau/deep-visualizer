20277287538ed466fb6434fbb795c4f8bfcab037
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-17 22:07:10 +0200

    Dummy activation function router (#142)

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from routers import access_token
+from routers.core import activation_function
 
 app = FastAPI()
 
 app.include_router(access_token.router)
+app.include_router(activation_function.router)
