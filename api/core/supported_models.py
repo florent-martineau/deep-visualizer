e90f0a8ba827776c4eef9a0231c13cf96b4eed47
Florent Martineau <ftecmartineau@gmail.com>
2025-09-17 21:10:19 +0200

    ...

--- api/core/supported_models.py
+++ api/core/supported_models.py
@@ -1,4 +1,4 @@
-from typing import Final, Set
+from typing import Final, List
 
 from pydantic import BaseModel, Field
 
@@ -12,13 +12,9 @@ class SupportedModel(BaseModel):
     )
 
 
-SUPPORTED_MODELS: Final[Set[SupportedModel]] = set(
-    [
-        SupportedModel(id="openai-community/openai-gpt", activation_function="gelu"),
-        SupportedModel(id="openai-community/gpt2", activation_function="gelu_new"),
-        SupportedModel(id="openai/gpt-oss-20b", activation_function="silu"),
-        SupportedModel(
-            id="mistralai/Mistral-7B-Instruct-v0.3", activation_function="silu"
-        ),
-    ]
-)
+SUPPORTED_MODELS: Final[List[SupportedModel]] = [
+    SupportedModel(id="openai-community/openai-gpt", activation_function="gelu"),
+    SupportedModel(id="openai-community/gpt2", activation_function="gelu_new"),
+    SupportedModel(id="openai/gpt-oss-20b", activation_function="silu"),
+    SupportedModel(id="mistralai/Mistral-7B-Instruct-v0.3", activation_function="silu"),
+]
