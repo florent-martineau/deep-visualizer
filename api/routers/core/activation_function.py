628a28ec2bb09fa66616392277a62e9a9ceb6e85
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-18 14:29:14 +0200

    Activation function endpoint: add min, max, and step parameters (#146)

--- api/routers/core/activation_function.py
+++ api/routers/core/activation_function.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 
 from core.activation_function import SUPPORTED_ACTIVATION_FUNCTIONS
 from utils.logs.logger import logger
@@ -10,11 +10,20 @@ router = APIRouter()
     "/activation-function/{activation_function}",
     description="This function takes as input a range (min, max) and a step. "
     "For each value in this interval, it will apply the activation function, "
-    "and return all the associated results. "
+    "and return all the associated activations. "
     "This allows you to plot the activation function.",
     status_code=200,
 )
-async def get_activation_function(activation_function: str):
+async def get_activation_function(
+    activation_function: str,
+    min: float = Query(description="Minimum value to generate activations from."),
+    max: float = Query(description="Maximum value to generate activations from."),
+    step: float = Query(
+        description="Step between each input value. "
+        "For instance, a step of 0.1 would generate activations for "
+        "inputs -1.0, -0.9, ..."
+    ),
+):
     logger.info(
         "Retrieving activation function",
         extra={"activation_function": activation_function},
--- api/routers/core/activation_function_test.py
+++ api/routers/core/activation_function_test.py
@@ -1,4 +1,4 @@
-from typing import get_args
+from typing import Dict, get_args
 
 import pytest
 from fastapi.testclient import TestClient
@@ -7,16 +7,28 @@ from core.activation_function import SUPPORTED_ACTIVATION_FUNCTIONS, ActivationF
 from main import app
 
 
-def _make_request(name: str):
+def _make_request(
+    name: str, min: float | None = -1, max: float | None = 1, step: float | None = 0.1
+):
     client = TestClient(app)
 
-    response = client.get(f"/activation-function/{name}")
+    params: Dict[str, float] = {}
+    if min is not None:
+        params["min"] = min
+
+    if max is not None:
+        params["max"] = max
+
+    if step is not None:
+        params["step"] = step
+
+    response = client.get(f"/activation-function/{name}", params=params)
     return response
 
 
 def should_return_404_if_activation_function_is_not_supported():
     name = "does-not-exist"
-    response = _make_request(name)
+    response = _make_request(name=name)
 
     assert name not in SUPPORTED_ACTIVATION_FUNCTIONS
     assert response.status_code == 404
@@ -24,7 +36,33 @@ def should_return_404_if_activation_function_is_not_supported():
 
 @pytest.mark.parametrize("name", SUPPORTED_ACTIVATION_FUNCTIONS)
 def should_not_return_404_if_activation_function_is_supported(name: str):
-    response = _make_request(name)
+    response = _make_request(name=name)
 
     assert name in get_args(ActivationFunction)
     assert response.status_code != 404
+
+
+@pytest.mark.parametrize(
+    "has_min, has_max, has_step",
+    [
+        (has_min, has_max, has_step)
+        for has_min in [True, False]
+        for has_max in [True, False]
+        for has_step in [True, False]
+    ],
+)
+def should_not_return_422_if_mandatory_parameter_is_missing(
+    has_min: bool, has_max: bool, has_step: bool
+):
+    if has_min and has_max and has_step:
+        pytest.skip("All mandatory parameters are provided")
+
+    activation_function = SUPPORTED_ACTIVATION_FUNCTIONS[0]
+    response = _make_request(
+        name=activation_function,
+        min=-1 if has_min else None,
+        max=1 if has_max else None,
+        step=0.1 if has_step else None,
+    )
+
+    assert response.status_code == 422
