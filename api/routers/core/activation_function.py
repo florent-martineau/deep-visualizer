33cb0d99c6a704b8a86af8c22693055d52772b50
Florent Martineau <ftecmartineau@gmail.com>
2025-09-20 17:49:11 +0200

    Activation function: generate range based on min, max, and step

--- api/routers/core/activation_function.py
+++ api/routers/core/activation_function.py
@@ -1,9 +1,21 @@
+from typing import List, Set
+
 from fastapi import APIRouter, HTTPException, Query
+from pydantic import BaseModel
 
 from core.activation_function import SUPPORTED_ACTIVATION_FUNCTIONS
 from utils.logs import logger
 
 router = APIRouter()
+
+
+class Activation(BaseModel):
+    input: float
+    activation: float
+
+
+class ActivationFunctionResponse(BaseModel):
+    activations: List[Activation]
 
 
 @router.get(
@@ -13,6 +25,7 @@ router = APIRouter()
     "and return all the associated activations. "
     "This allows you to plot the activation function.",
     status_code=200,
+    response_model=ActivationFunctionResponse,
 )
 async def get_activation_function(
     activation_function: str,
@@ -35,3 +48,15 @@ async def get_activation_function(
             detail=f"Activation function '{activation_function}' not found. "
             f"Valid functions are: {', '.join(SUPPORTED_ACTIVATION_FUNCTIONS)}",
         )
+
+    # TODO: return error if min >= max
+
+    inputs: Set[float] = set([min, max])
+    curr = min + step
+    while curr <= max:
+        inputs.add(round(curr, 6))
+        curr += step
+
+    return ActivationFunctionResponse(
+        activations=[Activation(input=input, activation=0.0) for input in inputs]
+    )
