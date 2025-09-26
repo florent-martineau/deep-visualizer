ddeade1e9ca39457f754612c5c336ad51b2a87d2
Florent Martineau <ftecmartineau@gmail.com>
2025-09-26 19:58:41 +0200

    ...

--- front/src/utils/logger.client.ts
+++ front/src/utils/logger.client.ts
@@ -2,5 +2,5 @@ import { env } from "@/env";
 import { Logtail } from "@logtail/browser";
 
 export const clientLogger = new Logtail(env.VITE_BETTERSTACK_TOKEN, {
-  endpoint: `https://${env.VITE_BETTERSTACK_INGESTING_HOST}`,
+	endpoint: `https://${env.VITE_BETTERSTACK_INGESTING_HOST}`,
 });
--- front/src/utils/logger.server.ts
+++ front/src/utils/logger.server.ts
@@ -2,5 +2,5 @@ import { env } from "@/env";
 import { Logtail } from "@logtail/node";
 
 export const serverLogger = new Logtail(env.BETTERSTACK_TOKEN, {
-  endpoint: `https://${env.BETTERSTACK_INGESTING_HOST}`,
+	endpoint: `https://${env.BETTERSTACK_INGESTING_HOST}`,
 });
