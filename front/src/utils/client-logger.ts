5b460045cf6cc6b60171fe13ee5b77519a9a33ea
Florent Martineau <ftecmartineau@gmail.com>
2025-09-26 10:09:30 +0200

    Frontend: setup betterstack logging

--- front/src/utils/client-logger.ts
+++ front/src/utils/client-logger.ts
@@ -2,5 +2,5 @@ import { env } from "@/env";
 import { Browser as LogtailBrowser } from "@logtail/js";
 
 export const clientLogger = new LogtailBrowser(env.VITE_BETTERSTACK_TOKEN, {
-  endpoint: `https://${env.VITE_BETTERSTACK_INGESTING_HOST}`,
+	endpoint: `https://${env.VITE_BETTERSTACK_INGESTING_HOST}`,
 });
--- front/src/utils/server-logger.ts
+++ front/src/utils/server-logger.ts
@@ -2,5 +2,5 @@ import { env } from "@/env";
 import { Node as LogtailServer } from "@logtail/js";
 
 export const serverLogger = new LogtailServer(env.BETTERSTACK_TOKEN, {
-  endpoint: `https://${env.BETTERSTACK_INGESTING_HOST}`,
+	endpoint: `https://${env.BETTERSTACK_INGESTING_HOST}`,
 });
