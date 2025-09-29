bc1191916133dd5b6999d832856679b1802c70af
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-09-29 16:50:44 +0200

    API: setup Sentry (#237)

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
+import sentry_sdk
 from fastapi import FastAPI
 
 from routers import access_token
 from routers.core import activation_function
+from utils.env import settings
+
+sentry_sdk.init(
+    dsn=settings().sentry_dsn,
+    send_default_pii=False,
+    traces_sample_rate=1.0,
+    profile_session_sample_rate=1.0,
+    profile_lifecycle="trace",
+    enable_logs=True,
+)
 
 app = FastAPI()
 
