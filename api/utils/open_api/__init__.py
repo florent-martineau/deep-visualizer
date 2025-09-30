5434769a257441af9f2e8dfe0c20e18ae64b2507
Florent Martineau <ftecmartineau@gmail.com>
2025-09-30 22:56:34 +0200

    ...

--- api/utils/open_api/__init__.py
+++
@@ -1,9 +0,0 @@
-import json
-
-from api.constants import API_BASE_PATH
-from api.main import app
-
-
-def generate_openapi_json(file_name: str = "openapi.json"):
-    with open(API_BASE_PATH / file_name, "w") as f:
-        json.dump(app.openapi(), f, indent=2)
