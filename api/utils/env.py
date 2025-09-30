de75187f22383750a09fa1a2c77acb3f5ca41341
Florent Martineau <ftecmartineau@gmail.com>
2025-09-30 21:57:31 +0200

    ...

--- api/utils/env.py
+++ api/utils/env.py
@@ -3,7 +3,7 @@ from typing import Literal
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-from api.constants import API_BASE_PATH
+from constants import API_BASE_PATH
 
 Environment = Literal["production", "development"]
 
