9d58d1e0dfa5b0e81c4c3043090a36744d3fa0c0
Florent Martineau <ftecmartineau@gmail.com>
2025-10-02 14:30:50 +0200

    Ensure that Orval-generated files are in line with OpenAPI spec

--- shared/openapi/codegen.test.ts
+++ shared/openapi/codegen.test.ts
@@ -4,6 +4,7 @@ import { getOrvalConfig } from 'orval.config';
 import os from 'os';
 import path from 'path';
 import { FRONT_OPENAPI_GENERATED_FILE_PATH } from 'shared/constants';
+import ts from 'typescript';
 import { describe, expect, onTestFinished, test } from 'vitest';
 
 describe('OpenAPI codegen', () => {
@@ -19,7 +20,14 @@ describe('OpenAPI codegen', () => {
 
         const existing = await fs.readFile(FRONT_OPENAPI_GENERATED_FILE_PATH, 'utf-8')
         const expected = await fs.readFile(expectedOutputPath, 'utf-8')
+
+        const parseTypeScript = (code: string) => {
+            return ts.createSourceFile('temp.ts', code, ts.ScriptTarget.Latest, true)
+        }
         
-        expect(expected).toBe(existing)
+        const existingAst = parseTypeScript(existing)
+        const expectedAst = parseTypeScript(expected)
+        
+        expect(expectedAst).toEqual(existingAst)
     })
 })
