5f9996c8bb7a48ff6e68ffedeb6fa4ad3b94769c
Florent Martineau <ftecmartineau@gmail.com>
2025-10-03 12:44:34 +0200

    API: prefix routes with /api

--- api/constants.py
+++ api/constants.py
@@ -7,6 +7,6 @@ ACCESS_TOKEN_COOKIE_NAME: Final[str] = "access_token"
 ACCESS_TOKEN_COOKIE_HEADER: Final[str] = "X-Access-Token"
 API_BASE_PATH: Path = Path(__file__).parent
 OPENAPI_JSON_FILE_NAME: Final[str] = "openapi.json"
-API_PATH_PREFIX: Annotated[
-    Final[str], Field(description="Base path prefix for all API endpoints")
+API_PATH_PREFIX: Final[
+    Annotated[str, Field(description="Base path prefix for all API endpoints")]
 ] = "/api"
