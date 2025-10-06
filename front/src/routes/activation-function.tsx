322378f90ce77ccad67b81e1a8183c16bbd7202b
Florent Martineau <ftecmartineau@gmail.com>
2025-10-06 18:24:46 +0200

    ...

--- front/src/routes/activation-function.tsx
+++ front/src/routes/activation-function.tsx
@@ -1,16 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useGetActivationFunctionApiActivationFunctionActivationFunctionNameGet } from "@/api";
+import { useGetActivationFunction } from "@/api";
 
 export const Route = createFileRoute("/activation-function")({
 	component: ActivationFunction,
 });
 
 function ActivationFunction() {
-	const { data } =
-		useGetActivationFunctionApiActivationFunctionActivationFunctionNameGet(
-			"gelu",
-			{ min: -1, max: 1, step: 0.5 },
-		);
+	const { data } = useGetActivationFunction("gelu", {
+		min: -1,
+		max: 1,
+		step: 0.5,
+	});
 
 	return <div>{JSON.stringify(data)}</div>;
 }
