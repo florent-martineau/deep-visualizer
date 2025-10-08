60bcb440c847cdae00d261751101a81970b8e8b3
Florent Martineau <ftecmartineau@gmail.com>
2025-10-08 13:35:03 +0200

    ...

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,28 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import {
-	NavigationMenu,
-	NavigationMenuContent,
-	NavigationMenuItem,
-	NavigationMenuLink,
-	NavigationMenuList,
-	NavigationMenuTrigger,
-} from "./ui/navigation-menu";
 
 export default function Header() {
-	return (
-		<NavigationMenu>
-			<NavigationMenuList>
-				<NavigationMenuItem>
-					<NavigationMenuTrigger>
-						<Link to="/activation-function">Activation Function</Link>
-					</NavigationMenuTrigger>
-					<NavigationMenuContent>
-						<NavigationMenuLink asChild>
-							<Link to="/activation-function">Activation Function</Link>
-						</NavigationMenuLink>
-					</NavigationMenuContent>
-				</NavigationMenuItem>
-			</NavigationMenuList>
-		</NavigationMenu>
-	);
+	return <Link to="/activation-function">Activation Function</Link>;
 }
