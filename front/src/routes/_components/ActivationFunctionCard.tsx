aefb2372ae58131d72e005fce88f5ce1d2a0d95b
Florent Martineau <ftecmartineau@gmail.com>
2025-10-11 17:14:40 +0200

    Neuron: more realistic sphere

--- front/src/routes/_components/ActivationFunctionCard.tsx
+++ front/src/routes/_components/ActivationFunctionCard.tsx
@@ -16,30 +16,30 @@ export const ActivationFunctionCard = () => {
 	const { route, staticData } = useRoute("/activation-functions");
 
 	return (
-		// <Link to={route.path}>
-		<Card className="w-92 hover:bg-primary/10">
-			<CardHeader>
-				<CardTitle>{staticData.title}</CardTitle>
-				<CardDescription>{staticData.description}</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
-					<ambientLight intensity={0.3} />
-					<directionalLight
-						position={[5, 5, 5]}
-						intensity={1}
-						castShadow
-						shadow-mapSize-width={2048}
-						shadow-mapSize-height={2048}
-					/>
-					<pointLight position={[-5, 5, -5]} intensity={0.5} />
+		<Link to={route.path}>
+			<Card className="w-92 hover:bg-primary/10">
+				<CardHeader>
+					<CardTitle>{staticData.title}</CardTitle>
+					<CardDescription>{staticData.description}</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
+						<ambientLight intensity={0.3} />
+						<directionalLight
+							position={[5, 5, 5]}
+							intensity={1}
+							castShadow
+							shadow-mapSize-width={2048}
+							shadow-mapSize-height={2048}
+						/>
+						<pointLight position={[-5, 5, -5]} intensity={0.5} />
 
-					<Neuron position={new Vector3(1, 2, 3)} />
+						<Neuron position={new Vector3(1, 2, 3)} />
 
-					<OrbitControls enableDamping />
-				</Canvas>
-			</CardContent>
-		</Card>
-		// </Link>
+						<OrbitControls />
+					</Canvas>
+				</CardContent>
+			</Card>
+		</Link>
 	);
 };
