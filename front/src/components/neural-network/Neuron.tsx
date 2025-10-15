bc70ffbb340a8fa037ed4f5045cb4a4d4fc2d29a
Florent Martineau <ftecmartineau@gmail.com>
2025-10-15 10:29:36 +0200

    Neuron : add activate imperative handler

--- front/src/components/neural-network/Neuron.tsx
+++ front/src/components/neural-network/Neuron.tsx
@@ -1,10 +1,27 @@
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 import type { Vector3 } from "three";
+
+export interface NeuronHandle {
+	activate: () => void;
+}
 
 type NeuronProps = {
 	position: Vector3;
 };
 
-export const Neuron = (props: NeuronProps) => {
+export const Neuron = forwardRef<NeuronHandle, NeuronProps>((props, ref) => {
+	const [isActivated, setIsActivated] = useState(false);
+
+	useImperativeHandle(ref, () => ({
+		activate: () => {
+			setIsActivated(true);
+		},
+	}));
+
+	useEffect(() => {
+		alert(isActivated);
+	}, [isActivated]);
+
 	return (
 		<mesh position={props.position} castShadow receiveShadow>
 			<sphereGeometry args={[1, 64, 64]} />
@@ -16,4 +33,4 @@ export const Neuron = (props: NeuronProps) => {
 			/>
 		</mesh>
 	);
-};
+});
