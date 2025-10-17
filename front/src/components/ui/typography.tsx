104bb0791301dc3dbaf5a89e541caf983e69dbf9
Florent Martineau <ftecmartineau@gmail.com>
2025-10-17 13:28:33 +0200

    Activation Functions list page: retrieve list of activation functions

--- front/src/components/ui/typography.tsx
+++ front/src/components/ui/typography.tsx
@@ -1,6 +1,6 @@
 export function H1({ children }: { children: React.ReactNode }) {
 	return (
-		<h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+		<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">
 			{children}
 		</h1>
 	);
--- front/src/routes/activation-functions/index.tsx
+++ front/src/routes/activation-functions/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useListActivationFunctions } from "@/api";
 import type { BreadcrumbMetadata } from "@/components/Header";
+import { H1, Lead, Muted, P } from "@/components/ui/typography";
+import { useRoute } from "@/hooks/useRoute";
 
 export const Route = createFileRoute("/activation-functions/")({
 	component: RouteComponent,
@@ -21,5 +24,16 @@ export const Route = createFileRoute("/activation-functions/")({
 });
 
 function RouteComponent() {
-	return <div>Hello "/activation-functions/"!</div>;
+	const route = useRoute("/activation-functions/");
+	const { data, isLoading } = useListActivationFunctions();
+
+	return (
+		<div className="flex flex-col gap-8 py-32">
+			<div className="flex flex-col items-center">
+				<H1>{route.staticData.title}</H1>
+				<Muted>{route.staticData.description}</Muted>
+			</div>
+			<p>{JSON.stringify(data)}</p>
+		</div>
+	);
 }
