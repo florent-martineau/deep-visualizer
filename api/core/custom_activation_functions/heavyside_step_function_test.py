b2a26da273703b827b11be95a5b5553daca9bb0f
Florent Martineau <ftecmartineau@gmail.com>
2025-10-22 19:48:02 +0200

    ...

--- api/core/custom_activation_functions/heavyside_step_function_test.py
+++ api/core/custom_activation_functions/heavyside_step_function_test.py
@@ -1,10 +1,12 @@
 import torch
 
-from api.core.custom_activation_functions.sinusoid import Sinusoid
+from api.core.custom_activation_functions.heavyside_step_function import (
+    HeavysideStepFunction,
+)
 
 
 def should_properly_compute_activation():
     tensor = torch.linspace(-5, 5, 100)
     expected = torch.heaviside(tensor, torch.tensor(0.0))
-    received = Sinusoid().forward(tensor)
+    received = HeavysideStepFunction().forward(tensor)
     assert torch.equal(expected, received)
