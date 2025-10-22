854e8928627847b0d740567dd12a764a0aee4954
Florent Martineau <ftecmartineau@gmail.com>
2025-10-22 12:19:09 +0200

    Activation Functions: add Sinusoid

--- api/core/custom_activation_functions/sinusoid.py
+++ api/core/custom_activation_functions/sinusoid.py
@@ -3,7 +3,7 @@ import torch
 
 class Sinusoid(torch.nn.Module):
     def __init__(self):
-        pass
+        super().__init__()
 
     def forward(self, x: torch.Tensor):
         return torch.sin(x)
