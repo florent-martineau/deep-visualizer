0572f3627ebadf820e5e9af551570f520728786d
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-10-22 23:24:28 +0200

    Activation Functions List page: improve responsiveness of grid (#468)

--- front/src/routes/activation-functions/index.tsx
+++ front/src/routes/activation-functions/index.tsx
@@ -37,7 +37,7 @@ function RouteComponent() {
 				<Muted>{loaderData.description}</Muted>
 			</div>
 
-			<div className="grid grid-cols-4 gap-0 w-fit border-l-[0.5px] border-t-[0.5px]">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-0 w-fit border-l-[0.5px] border-t-[0.5px]">
 				{activationFunctionsSortedByDisplayName?.map((activationFunction) => (
 					<ActivationFunctionNavigationCard
 						activationFunctionMetadata={activationFunction}
