3b93d30747a22a2fd269c638c1864c398f5f7bc8
Florent Martineau <ftecmartineau@gmail.com>
2025-10-27 21:08:51 +0100

    3D chart > Axis: center labels

--- front/src/components/3d/chart/axis.tsx
+++ front/src/components/3d/chart/axis.tsx
@@ -1,4 +1,4 @@
-import { Text3D } from "@react-three/drei";
+import { Center, Text3D } from "@react-three/drei";
 import type { Vector3 } from "three";
 import * as THREE from "three";
 import { TomorrowRegular } from "@/fonts/Tomorrow/regular";
@@ -8,8 +8,7 @@ export type AxisMetadata = {
 	from: Vector3;
 	to: Vector3;
 	labelOffset: Vector3;
-	anchorX?: "left";
-	anchorY?: "middle";
+	alignLeft?: true;
 };
 
 export const Axis = (props: AxisMetadata) => {
@@ -17,30 +16,26 @@ export const Axis = (props: AxisMetadata) => {
 	const length = props.from.length() + props.to.length();
 
 	return (
-		<>
+		<group>
 			<primitive
 				object={
 					new THREE.ArrowHelper(direction, props.from, length, "gray", 0.2, 0.1)
 				}
 			/>
 
-			<Text3D
-				position={[
-					props.to.x + props.labelOffset.x,
-					props.to.y + props.labelOffset.y,
-					props.to.z + props.labelOffset.z,
-				]}
-				font={TomorrowRegular}
-				size={0.1}
-				height={0.01}
+			<Center
+				position={props.to.add(props.labelOffset)}
+				right={props.alignLeft}
 			>
-				<meshStandardMaterial
-					color="#dddddd"
-					emissive="#dddddd"
-					emissiveIntensity={0.9}
-				/>
-				{props.label}
-			</Text3D>
-		</>
+				<Text3D font={TomorrowRegular} size={0.1} height={0.01}>
+					<meshStandardMaterial
+						color="#dddddd"
+						emissive="#dddddd"
+						emissiveIntensity={0.9}
+					/>
+					{props.label}
+				</Text3D>
+			</Center>
+		</group>
 	);
 };
