5f55e8f0e2f8b635d228c09d2f5eee139a55d787
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-10-28 18:55:59 +0100

    Highlight axis (#509)

--- front/src/components/3d/chart/axis.tsx
+++ front/src/components/3d/chart/axis.tsx
@@ -1,7 +1,10 @@
 import { Center, Text3D } from "@react-three/drei";
+import { forwardRef, useImperativeHandle, useState } from "react";
 import type { Vector3 } from "three";
 import * as THREE from "three";
 import { TomorrowRegular } from "@/fonts/Tomorrow/regular";
+import { colors } from "@/lib/colors";
+import type { WithHighlight } from "@/lib/highlight/types";
 
 export type AxisMetadata = {
 	label: string;
@@ -11,15 +14,24 @@ export type AxisMetadata = {
 	alignLeft?: true;
 };
 
-export const Axis = (props: AxisMetadata) => {
+export type AxisHandle = WithHighlight<{}>;
+
+export const Axis = forwardRef<AxisHandle, AxisMetadata>((props, ref) => {
+	const [isHighlighed, setIsHighlighted] = useState(false);
 	const direction = props.to.clone().sub(props.from);
 	const length = props.from.length() + props.to.length();
+
+	useImperativeHandle(ref, () => ({
+		toggleHighlight: (highlighted) => setIsHighlighted(highlighted),
+	}));
+
+	const color = isHighlighed ? colors.accent : "gray";
 
 	return (
 		<group>
 			<primitive
 				object={
-					new THREE.ArrowHelper(direction, props.from, length, "gray", 0.2, 0.1)
+					new THREE.ArrowHelper(direction, props.from, length, color, 0.2, 0.1)
 				}
 			/>
 
@@ -29,8 +41,8 @@ export const Axis = (props: AxisMetadata) => {
 			>
 				<Text3D font={TomorrowRegular} size={0.1} height={0.01}>
 					<meshStandardMaterial
-						color="#dddddd"
-						emissive="#dddddd"
+						color={color}
+						emissive={color}
 						emissiveIntensity={0.9}
 					/>
 					{props.label}
@@ -38,4 +50,4 @@ export const Axis = (props: AxisMetadata) => {
 			</Center>
 		</group>
 	);
-};
+});
