1a955127bb86a111647d12be2fea41bc5021d3d5
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-10-28 21:39:38 +0100

    Highlight Curve (#510)

--- front/src/components/3d/curve.tsx
+++ front/src/components/3d/curve.tsx
@@ -1,13 +1,24 @@
 import { Line } from "@react-three/drei";
+import { forwardRef, useImperativeHandle, useState } from "react";
 import type { Vector3 } from "three";
 import { colors } from "@/lib/colors";
+import type { WithHighlight } from "@/lib/highlight/types";
 
 type CurveProps = {
 	points: Vector3[];
 };
 
-export const Curve = (props: CurveProps) => {
+export type CurveHandle = WithHighlight<{}>;
+
+export const Curve = forwardRef<CurveHandle, CurveProps>((props, ref) => {
+	const [isHighlighed, setIsHighlighted] = useState(false);
+
+	useImperativeHandle(ref, () => ({
+		toggleHighlight: (highlighted) => setIsHighlighted(highlighted),
+	}));
+
 	if (props.points.length === 0) return;
 
-	return <Line points={props.points} color={colors.accent} lineWidth={3} />;
-};
+	const color = isHighlighed ? colors.accent : "gray";
+	return <Line points={props.points} color={color} lineWidth={3} />;
+});
