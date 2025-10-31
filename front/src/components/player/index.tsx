cbb9cee317e07af51c501bcf6e618db2d3f98a4d
Florent Martineau <ftecmartineau@gmail.com>
2025-10-31 19:07:07 +0100

    Player: responsive layout

---
+++ front/src/components/player/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { Vector3 } from "three";
+import { ThreeDimensionsCanvas } from "../3d/3d-canvas";
+import { Curve } from "../3d/curve";
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup,
+} from "../ui/resizable";
+
+export const Player = () => {
+	const isMobile = useMediaQuery("(max-width: 767px)");
+
+	if (isMobile) {
+		return (
+			<ResizablePanelGroup direction="vertical">
+				<ResizablePanel defaultSize={75}>
+					<ThreeDimensionsCanvas isRotating={false} isRunning={true}>
+						<Curve points={[new Vector3(-1, 0, 0), new Vector3(1, 0, 0)]} />
+					</ThreeDimensionsCanvas>
+				</ResizablePanel>
+
+				<ResizableHandle />
+
+				<ResizablePanel
+					defaultSize={25}
+					minSize={15}
+					maxSize={75}
+					className="pt-6"
+				>
+					Foo
+				</ResizablePanel>
+			</ResizablePanelGroup>
+		);
+	}
+
+	return (
+		<ResizablePanelGroup direction="horizontal">
+			<ResizablePanel defaultSize={75}>
+				<ThreeDimensionsCanvas isRotating={false} isRunning={true}>
+					<Curve points={[new Vector3(-1, 0, 0), new Vector3(1, 0, 0)]} />
+				</ThreeDimensionsCanvas>
+			</ResizablePanel>
+
+			<ResizableHandle />
+
+			<ResizablePanel
+				defaultSize={25}
+				minSize={15}
+				maxSize={75}
+				className="pl-6"
+			>
+				Foo
+			</ResizablePanel>
+		</ResizablePanelGroup>
+	);
+};
