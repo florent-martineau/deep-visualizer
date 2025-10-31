fd3532229d641307619df0e73aed978c4a92a9fe
Florent Martineau <ftecmartineau@gmail.com>
2025-10-31 17:08:57 +0100

    Player: split screen into the transcript and the scene

--- front/src/routes/__root.tsx
+++ front/src/routes/__root.tsx
@@ -51,7 +51,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			<head>
 				<HeadContent />
 			</head>
-			<body className="h-full px-6 lg:px-12 pt-6">
+			<body className="h-full px-6 lg:px-12 py-6">
 				<Header className="absolute" />
 
 				<div className="h-full pt-10">{children}</div>
--- front/src/routes/learn.tsx
+++ front/src/routes/learn.tsx
@@ -1,4 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Vector3 } from "three";
+import { ThreeDimensionsCanvas } from "@/components/3d/3d-canvas";
+import { Curve } from "@/components/3d/curve";
+import { Separator } from "@/components/ui/separator";
 import type { LoaderData } from "@/lib/router/types";
 
 export const Route = createFileRoute("/learn")({
@@ -18,5 +22,15 @@ export const Route = createFileRoute("/learn")({
 });
 
 function RouteComponent() {
-	return <div>Hello "/learn"!</div>;
+	return (
+		<div className="h-full flex gap-12">
+			<ThreeDimensionsCanvas isRotating={false} isRunning={true}>
+				<Curve points={[new Vector3(-1, 0, 0), new Vector3(1, 0, 0)]} />
+			</ThreeDimensionsCanvas>
+
+			<Separator orientation="vertical" />
+
+			<div className="w-1/4">Foo</div>
+		</div>
+	);
 }
