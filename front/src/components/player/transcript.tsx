cda913bf5b312658470e036f3acad2bba79d459f
florent-martineau <88397419+florent-martineau@users.noreply.github.com>
2025-11-01 11:07:54 +0100

    Transcript: support Markdown (#557)

--- front/src/components/player/transcript.tsx
+++ front/src/components/player/transcript.tsx
@@ -1,6 +1,30 @@
+import ReactMarkdown from "react-markdown";
+import { H1, H2, P } from "../ui/typography";
+
 /**
  * Responsible for displaying the transcript of an explanation
  */
 export const Transcript = () => {
-	return <div>Foo</div>;
+	const markdownContent = `# Title
+
+Foo bar
+
+## Subtitle
+
+* This is
+* A list
+* Including
+* [A link](/activation-functions)
+	`;
+	return (
+		<ReactMarkdown
+			components={{
+				h1: ({ node, ...props }) => <H1 {...props}>{props.children}</H1>,
+				h2: ({ node, ...props }) => <H2 {...props}>{props.children}</H2>,
+				p: ({ node, ...props }) => <P {...props}>{props.children}</P>,
+			}}
+		>
+			{markdownContent}
+		</ReactMarkdown>
+	);
 };
